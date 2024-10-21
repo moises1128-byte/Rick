618cd9607d05769f2d835d5c388e45cca5f14895
moises <1128moises@gmail.com>
2024-10-21 18:17:49 -0400

    Feat: login, register and loading page

---
+++ src/app/auth/register/page.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useToast } from "@/hooks/use-toast";
+import Link from "next/link";
+
+const Register = () => {
+  const { toast } = useToast();
+
+  const emailRegExp =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  const formSchema = z.object({
+    email: z
+      .string({ required_error: "Email is required" })
+      .regex(new RegExp(emailRegExp), { message: "The email is not valid" }),
+    password: z.string({ required_error: "Password is required" }).min(6, {
+      message: "Password must be at least 6 characters.",
+    }),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    toast({
+      description: "Your message has been sent.",
+    });
+    console.log(values);
+  }
+
+  return (
+    <main className="bg-white flex w-screen	h-screen justify-center items-center">
+      <div className="w-1/4	flex flex-col items-start p-6 gap-6 bg-white rounded-2xl border border-gray-800 border-opacity-20 shadow-sm	">
+        <h3 className="mt-6 text-gray-700 text-3xl font-bold leading-10 font-[family-name:var(--font-geist-mono)]">
+          Register
+        </h3>
+        <div className="mt-10 flex flex-col gap-y-10 w-full items-center">
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="space-y-8 w-full"
+            >
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="shadcn" {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input type="password" placeholder="shadcn" {...field} />
+                    </FormControl>
+
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Link
+                className="flex w-fit justify-center hover:opacity-50"
+                href={"/auth/login"}
+              >
+                <span>Have an account ?</span>
+              </Link>
+
+              <Button type="submit">Submit</Button>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Register;
